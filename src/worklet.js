72df8c1030043d13e02d56758e05404ecbe220d0
Fishbite <43952420+Fishbite@users.noreply.github.com>
2022-07-05 11:01:46 +0100

    User interaction added to create the AudioContext

--- src/worklet.js
+++ src/worklet.js
@@ -1,16 +1,26 @@
 console.log("Worklet.js connected");
-/* a complete example of GainNode built on top of AudioWorkletNode and AudioWorkletProcessor. */
-const actx = new AudioContext();
+/* a complete example of GainNode built on top of AudioWorkletNode and AudioWorkletProcessor. Dependends on `gain-processor.js */
+const Btn = document.getElementById("btn");
+Btn.addEventListener("click", worklet, false);
 
-// load module  via AudioWorklet
-actx.audioWorklet.addModule("src/gain-processor.js").then(() => {
-  let osc = new OscillatorNode(actx);
+function worklet(e) {
+  Btn.innerHTML = "STOP";
 
-  // after the mmodule is loaded, an AudioWorkletNode
-  // can be constructed
-  let gainWorkletNode = new AudioWorkletNode(actx, "gain-processor");
+  const Actx = new AudioContext();
 
-  // audioWorkletNode can be interoperable with other native nodes
-  osc.connect(gainWorkletNode).connect(actx.destination);
-  osc.start();
-});
+  // load module  via AudioWorklet
+  Actx.audioWorklet.addModule("src/gain-processor.js").then(() => {
+    let osc = new OscillatorNode(Actx);
+
+    // after the mmodule is loaded, an AudioWorkletNode
+    // can be constructed
+    let gainWorkletNode = new AudioWorkletNode(Actx, "gain-processor");
+
+    // audioWorkletNode can be interoperable with other native nodes
+    osc.connect(gainWorkletNode).connect(Actx.destination);
+    osc.start();
+    alert(
+      "Thanks! That's all this test does at the moment,  please refresh the page to stop that noise!!!"
+    );
+  });
+}
