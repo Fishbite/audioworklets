fcf455028f688735e917124e5b24851742d157a6
Fishbite <43952420+Fishbite@users.noreply.github.com>
2022-07-18 10:51:07 +0100

    created undying osc

--- src/worklet.js
+++ src/worklet.js
@@ -1,16 +1,15 @@
 console.log("Worklet.js connected");
-/* a complete example of GainNode built on top of AudioWorkletNode and AudioWorkletProcessor. Dependends on `gain-processor.js */
+/* a complete example of GainNode built on top of AudioWorkletNode and AudioWorkletProcessor. Depends on `gain-processor.js */
 const Btn = document.getElementById("btn");
-Btn.addEventListener("click", showAlert, false);
+Btn.addEventListener("click", runWorklet, false);
 
-function showAlert() {
-  runWorklet();
-}
+// function showAlert() {
+//   runWorklet();
+// }
 
 function runWorklet() {
-  Btn.innerHTML = "Running";
-
   const Actx = new AudioContext();
+  Btn.innerHTML = "Running";
 
   // load module  via AudioWorklet
   Actx.audioWorklet.addModule("src/gain-processor.js").then(() => {
@@ -24,18 +23,27 @@ function runWorklet() {
 
     // audioWorkletNode can be interoperable with other native nodes
     osc.connect(gainWorkletNode).connect(Actx.destination);
+    console.log(osc.context.state);
     window.addEventListener("keydown", play, false);
+    osc.playing = false;
 
     function play(e) {
       window.addEventListener("keyup", stop, false);
-      osc.start();
-      function stop(e) {
-        console.log(gainParam);
-        gainParam.setValueAtTime(1, Actx.currentTime);
-        gainParam.exponentialRampToValueAtTime(0.0000001, Actx.currentTime + 5);
-        gainParam.linearRampToValueAtTime(0, Actx.currentTime + 6);
 
-        osc.stop(Actx.currentTime + 3);
+      gainParam.setValueAtTime(1, Actx.currentTime);
+
+      if (osc.playing === false) {
+        osc.start();
+        osc.playing = true;
+      }
+      console.log(osc.context.state);
+      function stop(e) {
+        // console.log(gainParam);
+        // gainParam.setValueAtTime(1, Actx.currentTime);
+        gainParam.linearRampToValueAtTime(0, Actx.currentTime + 0);
+        // gainParam.linearRampToValueAtTime(0, Actx.currentTime + 6);
+
+        // osc.stop(Actx.currentTime + 3);
       }
     }
     // osc.start();
